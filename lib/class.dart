9ad4372181c3d141a0fc7a467b0d9badb7066e83
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-13 07:54:53 +0700

    menambahkan script untuk memanggil nama dari si Manusia tersebut

--- lib/class.dart
+++ lib/class.dart
@@ -2,8 +2,14 @@ class Manusia {
   // deklarasi atribut/properti
   String nama = ""; //atribut nama bertipe string dan nilai awal null
 
+  // pembuatan konstruktor, nama konstruktor adalah nama class
+  Manusia(nm) {
+    // nm a/ sebuah parameter yang dipergunakan untuk menampung data
+    this.nama = nm; // atribut nama memiliki data dari parameter nm
+  }
+
   // deklarasi method/fungsi/berhavior/tingkah laku bernama makan
   void makan() {
-    print('makan nasi');
+    print('$nama makan nasi'); //tambahkan var nama pada string makan
   }
 }
