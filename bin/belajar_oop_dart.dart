1438b5120a80aa3f2e94774eb83c4d7b366cda4a
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-13 07:57:42 +0700

    menambahkan data parameter  di class manusia

--- bin/belajar_oop_dart.dart
+++ bin/belajar_oop_dart.dart
@@ -4,7 +4,8 @@ import 'package:belajar_oop_dart/class.dart'; //import library class.dart
 // awal program utama
 void main() {
   // instansiasi/instance object adalah proses pembuatan object dari sebuah class
-  var programmer = new Manusia(); //membuat object dari class manusia
+  var programmer =
+      new Manusia('Syecih Khalil'); //membuat object dari class manusia
   programmer
       .makan(); //object programmer memanggil method makan di class Manusia
 }//akhir program utama
