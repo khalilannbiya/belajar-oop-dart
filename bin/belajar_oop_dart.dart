7d1c941cacd4facd23530f7dd381f32116771c8c
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-13 07:40:36 +0700

    menambahka script untuk bisa mengakses class manusia

--- bin/belajar_oop_dart.dart
+++ bin/belajar_oop_dart.dart
@@ -1,1 +1,8 @@
 import 'package:belajar_oop_dart/belajar_oop_dart.dart' as belajar_oop_dart;
+import 'package:belajar_oop_dart/class.dart'; //import library class.dart
+
+// awal program utama
+void main() {
+  // instansiasi/instance object adalah proses pembuatan object dari sebuah class
+  var progammer = new Manusia(); //membuat object dari class manusia
+}//akhir program utama
