37ac55b66fa29fc70c3c164c8efade535a64de1b
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-13 07:34:56 +0700

    menambahkan script pendeklrasian atribut dan method

---
+++ lib/class.dart
@@ -0,0 +1,9 @@
+class Manusia {
+  // deklarasi atribut/properti
+  String nama = ""; //atribut nama bertipe string dan nilai awal null
+
+  // deklarasi method/fungsi/berhavior/tingkah laku bernama makan
+  void makan() {
+    print('makan nasi');
+  }
+}
