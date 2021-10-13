43106e5f3890ee06932dd5a50249da093200008a
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-13 07:47:28 +0700

    menambahkan script untuk memanggil mehthod makan

--- bin/belajar_oop_dart.dart
+++ bin/belajar_oop_dart.dart
@@ -4,5 +4,7 @@ import 'package:belajar_oop_dart/class.dart'; //import library class.dart
 // awal program utama
 void main() {
   // instansiasi/instance object adalah proses pembuatan object dari sebuah class
-  var progammer = new Manusia(); //membuat object dari class manusia
+  var programmer = new Manusia(); //membuat object dari class manusia
+  programmer
+      .makan(); //object programmer memanggil method makan di class Manusia
 }//akhir program utama
